f777878a4752dec6888d85f78d0b3c783c27411d
aminaaman <aminaaman07@gmail.com>
2019-12-10 00:49:19 +0600

    first commit

---
+++ foodstore-back-end/api/middleware/authenticate.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+
+    try{
+
+        const token = req.headers["auth-token"];
+        const decoded = jwt.verify(token, 'mysecretkey');
+        next();
+
+    }catch(error){
+        res.status(500).json({
+            error: error
+        });
+    }
+   
+
+}
+
+module.exports = authenticate;
